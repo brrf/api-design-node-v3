ac71e3ca1e828e3a17bf2e9a95be60b0ebb30b47
Moshe Praver <moshepraver@gmail.com>
2019-03-25 13:34:54 -0500

    exercise 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -1,5 +1,17 @@
 import { Router } from 'express'
 
 const router = Router()
+const controller = (req, res) => {
+	res.send({response: 'got it!'})
+}
+
+router.route('/')
+	.get(controller)
+	.post(controller)
+
+router.route('/:id')
+	.put(controller)
+	.delete(controller)
+	.get(controller)
 
 export default router
